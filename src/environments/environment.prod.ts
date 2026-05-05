e05078b4134933f772906edb08a096e2a5f418df
Paulo Winicius Silva Junior <paulo.winicius.jr@gmail.com>
2026-05-05 17:16:21 -0300

    features: correcao do environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://diecast-production.up.railway.app/'
+  //apiUrl: 'https://diecast-production.up.railway.app/'
+  apiUrl: 'https://diecast-back.onrender.com/'
 };
