081374638eba4e9a65472c76348c85a53831d1be
Paulo Winicius Silva Junior <paulo.winicius.jr@gmail.com>
2026-05-08 11:03:23 -0300

    refactor color vehicle no dashboard

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -14,6 +14,7 @@ import { ColorConverter } from '../../../shared/color.converter';
 export class AdminDashboardComponent implements OnInit {
   data!: AdminDashboardData;
   loading = true;
+  ColorConverter = ColorConverter;
 
   constructor(private route: ActivatedRoute) {}
 
